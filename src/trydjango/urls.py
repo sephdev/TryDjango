a45af39d293a2df20afc082bad2e28baf0188e34
Joseph Tandingan <josephdevapps@gmail.com>
2019-01-10 14:45:05 +0800

    Default Homepage to Custom Homepage

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from pages.views import home_view
+
 urlpatterns = [
+    url(r'^$', home_view, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
